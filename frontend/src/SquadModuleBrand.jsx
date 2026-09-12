c8be07a28de5154f4b51a0bec3b5aed4407252aa
MatthiastheHead <matthias@head-mail.com>
2026-09-12 19:53:43 +0200

    Version 9.1: Einheitliches SquadHQ-Design in allen Bereichen

---
+++ frontend/src/SquadModuleBrand.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function SquadModuleBrand() {
+  return <div className="squad-module-brand" aria-label="SquadHQ">
+    <img src="/brand/squadhq-logo.png" alt="" width="44" height="44" />
+    <span>Squad<strong>HQ</strong></span>
+  </div>;
+}
